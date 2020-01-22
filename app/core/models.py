ed294d2b7eaa675756bf8986386b431b71ccda8b
agongora@omnibnk.com <josangelo.jose@gmail.com>
2020-01-22 11:05:05 -0500

    add changes to create api user

--- app/core/models.py
+++ app/core/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, \
-                                       PermissionsMixin
+                                        PermissionsMixin
 
 
 class UserManager(BaseUserManager):
@@ -26,7 +26,7 @@ class UserManager(BaseUserManager):
 
 
 class User(AbstractBaseUser, PermissionsMixin):
-    """Custom user model that suppors using email instead of username"""
+    """Custom user model that supports using email instead of username"""
     email = models.EmailField(max_length=255, unique=True)
     name = models.CharField(max_length=255)
     is_active = models.BooleanField(default=True)
